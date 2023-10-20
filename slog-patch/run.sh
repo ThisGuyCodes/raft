c2dc48bff3724c582dc99f3575f581b3fdc14db7
Travis Johnson <travis@thisguy.codes>
2023-10-19 23:49:15 -0400

    rely on CWD being inside the repo we want to change

--- slog-patch/run.sh
+++ slog-patch/run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-declare -r SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-cd "$SCRIPT_DIR/.."
+# this must be run when the CWD is the repo we want to change
+declare -r REPO_DIR="$(git rev-parse --show-toplevel)"
+cd "$REPO_DIR"
 
 semgrep scan -c slog-patch --autofix "$@"
 # for some reason I can't get them all in one go
